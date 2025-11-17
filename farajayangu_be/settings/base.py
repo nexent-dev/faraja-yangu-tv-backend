37b0d4d42a43c4a9cc378fefaf3aef6efd0c0611
Joe <josephmiley61@gmail.com>
2025-11-17 07:43:33 +0300

    Fixed cors error

--- farajayangu_be/settings/base.py
+++ farajayangu_be/settings/base.py
@@ -60,7 +60,7 @@ ALLOWED_HOSTS = ['*'] if DEBUG else (
     "https://farajayangutv.co.tz",
 )
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = DEBUG
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = (
     'http://127.0.0.1:5713',
