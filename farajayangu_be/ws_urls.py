60eee050e5d1789122849b513af68b64df486597
Joe <josephmiley61@gmail.com>
2025-12-12 12:03:24 +0300

    Setup Socket Connection For Video Progress

--- farajayangu_be/ws_urls.py
+++ farajayangu_be/ws_urls.py
@@ -4,4 +4,4 @@ from apps.streaming.socket.consumers import VideoProcessorConsumer
 # WebSocket URL patterns used by Channels' URLRouter
 ws_urlpatterns = [
     path("socket/stream/progress/<int:video_uid>/", VideoProcessorConsumer.as_asgi()),
-]
+] 
